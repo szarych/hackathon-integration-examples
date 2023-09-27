05a3f637ac662e2e0a61c31bc4e5de0e641876ea
bottjert <thomas.bottjer@fisglobal.com>
2023-09-27 10:27:06 +0100

    feat: add vt headers and error handling

--- server/index.js
+++ server/index.js
@@ -6,22 +6,27 @@ const port = process.env.PORT || 9001;
 const PROD = process.env.NODE_ENV === 'production';
 
 const vtEndpoint = 'https://try.access.worldpay.com/verifiedTokens/cardOnFile';
-// const tokenHeaders = {
-//     'Content-Type': 'application/vnd.worldpay.tokens-v3.hal+json',
-//     Accept: 'application/vnd.worldpay.tokens-v3.hal+json',
-// }
+const tokenHeaders = {
+    'Content-Type': 'application/vnd.worldpay.verified-tokens-v3.hal+json',
+    Accept: 'application/vnd.worldpay.verified-tokens-v3.hal+json',
+    Authorization: `Basic ${credentials}`,
+}
 
 if (PROD) {
     app.use('/', express.static('dist'));
 }
 
 app.post('/api/addCard', async (req, res) => {
-    const reqBody = {
+    try {
+        const reqBody = {
+        }
+        const response = await axios.post(vtEndpoint, reqBody, { headers: tokenHeaders })
+        res.send(response);
+    } catch (error) {
+        console.log(error)
+        res.status(error.response.status)
+        res.send(error.response.data)
     }
-    const response = await axios.post(vtEndpoint, reqBody
-        // , { headers: tokenHeaders }
-    )
-    res.send(response);
 });
 
 app.listen(port, () => {
