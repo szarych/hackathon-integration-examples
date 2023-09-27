47a2c4010356fd7243433eecd913af0dfffda650
szarych <sebastian.zarych@fisglobal.com>
2023-09-27 12:02:57 +0100

    feat: add card

--- client/app.js
+++ client/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Card } from './components/checkout';
 import request from 'axios';
 
 import './app.css';
@@ -25,41 +26,11 @@ class App extends Component {
 
     render() {
         return (
-            <div>
-                <div className="highlight" />
-                <div className="container">
-                    <h1>{this.state.message}</h1>
-                    <div className="info">
-                        <h2>
-                            Development
-                        </h2>
-
-                        <p>
-                            To get started with the client-side, edit <code>client/app.js</code>
-                        </p>
-
-                        <p>
-                            To get started with the server-side, edit <code>server/index.js</code>
-                        </p>
-
-                        <p>
-                            To run the development server, run <code>npm run start:dev</code>
-                        </p>
-
-                        <h2>
-                            Production
-                        </h2>
-
-                        <p>
-                            To build the project for production, run <code>npm run start</code>
-                        </p>
-
-                        <div className="t-center">
-                            Built with ❤️ by <a href="https://twitter.com/AbinavSeelan">Abinav Seelan</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <section className="container">
+                <section className="wrapper">
+                    <Card />
+                </section>
+            </section>
         );
     }
 }
